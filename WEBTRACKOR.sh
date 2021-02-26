71536aacea6fe123631d250a540cc1264748a1c4
anynomouscheetah <79717256+anynomouscheetah@users.noreply.github.com>
2021-02-27 00:31:54 +0500

    Add files via upload

---
+++ WEBTRACKOR.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+
+banner() {
+
+clear
+printf "      \e[33;1m ______      _     _     _               \e[0m\n"
+printf "      \e[33;1m|___  /     | |   (_)   | |              \e[0m\n"
+printf "      \e[33;1m   / / _ __ | |__  _ ___| |__   ___ _ __ \e[0m\n"
+printf "      \e[32;1m  / /\e[33;1m | '_ \| '_ \| / __| '_ \ / _ \ '__|\e[0m\n"
+printf "      \e[32;1m / /__| |_) | | | | \__ \ | | |  __/ |   \e[0m\n"
+printf "      \e[32;1m/_____| .__/|_| |_|_|___/_| |_|\___|_|   \e[0m\n"
+printf "      \e[33;1m      | |                                \e[0m\n"
+printf "      \e[33;1m      |_|           \e[1;97m    Version 2.1 Beta      \e[0m\n"
+printf "\n"
+printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by htr-tech (tahmid.rayat)\e[0m\n"
+printf "\n"
+
+}
+
+menu() {
+
+banner
+printf " \e[1;31m[\e[0m\e[1;77m______\e[0m\e[1;31m]\e[0m\e[1;93m  ENTER WEBSITE LINK HERE     \e[0m\e[1;31m[\e[0m\e[1;77m\e[0m\e[1;31m]\e[0m\e[1;93m      \e[0m\e[1;31m[\e[0m\e[1;77m\e[0m\e[1;31m]\e[0m\e[1;93m \e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' menu_option
+
+if [[ $menu_option == *.ngrok.io || $menu_option == *.ngrok.* || $menu_option == *.bit*.* || $menu_option == http://*@  || $menu_option == *.serv*.net || $menu_option == *127.0.0.1:* || $menu_option == *127.0.0.1 || $menu_option == *.ngrok. || $menu_option == *.ngrok.k || $menu_option == *.ngrok.k || $menu_option == *.ngrok.k ]]; then
+facebook
+printf "\e[0m\n"
+printf " \e[0m\e[1;42m Thanks for Using This Tool !!\e[0m  \e[1;44m Visit https://github.com/balibahi or more\e[0m\n"
+printf "\e[0m\n"
+exit 1
+
+else
+printf "\n\n \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+
+}
+ok(){
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m THANKS FOR USING THIS TOOL \e[0m\n"
+}
+facebook(){
+
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Sorry This link is not secure \e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Enter OK,ok for next: \e[0m\e[1;96m\en' fb_option
+
+if [[ $fb_option == OK || $fb_option == ok ]]; then
+ok
+else
+printf "\n\n  \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+
+}
+ok(){
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m THANKS FOR USING THIS TOOL \e[0m\n"
+}
+
+banner
+reqs
+menu
